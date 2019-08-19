8b67fe171a64262dfe0ff3bdf5eb8928eb9adf0e
benson19 <tomaffew@me.com>
2019-08-19 19:43:34 -0400

    first

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,42 @@
 class ProjectsController < ApplicationController
+    before_action :find_project, only: [:show]
+
+    def index
+        @projects = Project.all
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def edit
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def find_project
+        @project = Project.find(params[:id])
+    end
+
+    def project_params
+        params.require(:project).permit(:title, :pay, :skill_id)
+    end
+
 end
